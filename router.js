e65fa245e8604cddd271075c615b75b672cb9d33
byung <byungklee4@gmail.com>
2014-12-25 15:21:25 -0800

    updates

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
-function route(pathname) {
-	console.log("About to route a request for " + pathname);
+var querystring = require("querystring");
+
+function route(pathname, query) {
+	console.log("About to route a request for " + pathname + " " + query);
+	if(pathname == "/add") {
+		console.log(querystring.parse(query)["data"]);
+		var temp = querystring.parse(query)["data"];
+		//temp = '[' + temp + ']';
+		var jsonObj = JSON.parse(temp);
+		console.log(jsonObj.data1);
+	}
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -3,16 +3,20 @@ var url = require("url");
 
 function start(route) {
   function onRequest(request, response) {
+    if(request.method == 'POST') {
+    } else {
+    
     var pathname = url.parse(request.url).pathname;
+    var queryString = url.parse(request.url).query;
     console.log("Request " + pathname + " received.");
 
-    route(pathname);
+    route(pathname, queryString);
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
+    }
   }
-
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
 }
