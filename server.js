7ed474631d0be1941b97ff01f1d7b9c0c3a728c9
byung <byungklee4@gmail.com>
2014-12-26 15:24:19 -0800

    test

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ var url = require("url");
 
 function start(route) {
   function onRequest(request, response) {
-    if(request.method == 'POST') {
-    } else {
+  //   if(request.method == 'POST') {
+  //   } else {
     
-    var pathname = url.parse(request.url).pathname;
-    var queryString = url.parse(request.url).query;
-    console.log("Request " + pathname + " received.");
+  //   var pathname = url.parse(request.url).pathname;
+  //   var queryString = url.parse(request.url).query;
+  //   console.log("Request " + pathname + " received.");
 
-    //route(pathname, queryString);
+  //   //route(pathname, queryString);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-    }
-  }
+  //   response.writeHead(200, {"Content-Type": "text/plain"});
+  //   response.write("Hello World");
+  //   response.end();
+  //   }
+  // }
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+
   http.createServer(onRequest).listen(5000);
   console.log("Server has started.");
 }
